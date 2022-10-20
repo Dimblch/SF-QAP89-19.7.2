e182a1670bebdaeaa36a0d84ca58a35be8df8919
Dimblch <dimblch080@gmail.com>
2022-10-20 18:23:56 +0300

    PyCharm

--- api.py
+++ api.py
@@ -105,3 +105,46 @@ class PetFriends:
         except json.decoder.JSONDecodeError:
             result = res.text
         return status, result
+
+#######################################
+###   Два нереальзованных запроса   ###
+#######################################
+
+    def add_new_pet_simple(self, auth_key: json, name: str, animal_type: str, age: str) -> json:
+        """Метод отправляет (постит) на сервер данные о добавляемом питомце в упрощенном формате (без фото)
+        и возвращает статус запроса на сервер и результат в формате JSON с данными добавленного питомца"""
+
+        headers = {'auth_key': auth_key['key']}
+        data = {'name': name,
+                'animal_type': animal_type,
+                'age': age,
+                }
+
+        res = requests.post(self.base_url + 'api/create_pet_simple', headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+
+    def add_photo_to_pet(self, auth_key: json, pet_id: str, pet_photo: str) -> json:
+        """Метод отправляет (постит) на сервер фотографию к заведенному ранее питомцу и возвращает статус
+        запроса на сервер и результат в формате JSON с данными питомца"""
+
+        data = MultipartEncoder(
+            fields={
+                'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpeg')
+            })
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
+
+        res = requests.post(self.base_url + '/api/pets/set_photo/' + pet_id, headers=headers, data=data)
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
